2e6306d54a3b8c6d4d2387eb8962dd355f30ccda
Panther <152974820+pantherp19@users.noreply.github.com>
2024-06-21 20:50:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pravin Mane: React Developer
         </a>
       </header>
     </div>
